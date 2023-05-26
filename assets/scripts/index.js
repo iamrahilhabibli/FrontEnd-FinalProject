abbe0282b05b7afae3fb348d55b13ac30cc28f79
= <=>
2023-05-26 22:14:17 +0400

    slider speed added

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,8 +7,18 @@ new simpleParallax(clockImage, {
 const swiper = new Swiper(".swiper-container", {
   direction: "horizontal",
   loop: true,
+  slidesPerView: "auto",
+  initialSlide: 2,
+  speed: 2000,
+  spaceBetween: 32,
+  centeredSlides: true,
+  roundLengths: true,
   pagination: {
     el: ".swiper-pagination",
+  },
+  effect: "slide", // (e.g., 'slide', 'fade', 'cube', 'coverflow', 'flip')
+  fadeEffect: {
+    crossFade: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
