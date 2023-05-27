311d3dddd0debff4b9678d5b86c1bde39bbcfe4e
Rahil Habibli <rahilah@code.edu.az>
2023-05-27 14:37:15 +0400

    countdown js func added

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -18,3 +18,25 @@ var mySwiper = new Swiper(".swiper-container", {
   },
   loop: true,
 });
+
+const endDate = new Date();
+endDate.setDate(endDate.getDate() + 15);
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+  const now = new Date();
+  const timeDifference = endDate - now;
+
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+}
