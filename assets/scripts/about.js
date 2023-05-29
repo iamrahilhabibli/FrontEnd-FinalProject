9c43d415c9a897a87cece3170ab2a47e63da7871
= <=>
2023-05-29 22:29:36 +0400

    swiper slider done

---
+++ assets/scripts/about.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 5,
+  paginationClickable: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
