65dad01d985d1cd8615efb825d08374e711dd739
= <=>
2023-05-30 20:20:05 +0400

    blogcards images added

--- assets/scripts/blog.js
+++ assets/scripts/blog.js
@@ -1,15 +1,2 @@
-// let img2 = document.getElementsByClassName("parallaxmainimage");
-// new simpleParallax(img2, {
-//   overflow: true,
-// });
-
-// var image = document.getElementsByClassName("parallaxmainimage");
-// new simpleParallax(image, {
-//   scale: 1.5,
-// });
-
 var image = document.getElementsByClassName("parallaxmainimage");
-new simpleParallax(image, {
-  orientation: "right",
-});
-console.log(image);
+new simpleParallax(image);
