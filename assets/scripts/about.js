c513a0e896b27dc89edea0f00b8fdf3d12dd7464
Rahil Habibli <rahilah@code.edu.az>
2023-05-31 20:46:56 +0400

    basket local storage retriving func done

--- assets/scripts/about.js
+++ assets/scripts/about.js
@@ -46,3 +46,42 @@ sideBarCloseBtn.addEventListener("click", () => {
   sideBarModal.style.display = "none";
   sideBarModalDialog.style.display = "none";
 });
+
+function loadCart() {
+  const savedCart = JSON.parse(localStorage.getItem("shoppingCart"));
+  if (savedCart) {
+    cart = savedCart;
+  }
+}
+
+function displayCartItems() {
+  const productContainer = document.querySelector(".navbar-cart-product");
+
+  for (const item of cart) {
+    const itemContainer = document.createElement("div");
+    itemContainer.classList.add("item-container");
+
+    itemContainer.innerHTML = `
+      <a href=""><img class="itemimg" src="${item.image}" alt="" /></a>
+      <div class="item-content">
+        <a class="closebtn-deleteitem" href="#">
+          <button class="closebtn-deleteitem" type="button">
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+        </a>
+        <div class="pl-3">
+          <a class="navbar-cart-product" href="">${item.name}</a>
+          <small class="d-block text-muted">Quantity: ${item.count}</small>
+          <strong class="d-block text-sm">$${item.price.toFixed(2)}</strong>
+        </div>
+      </div>
+    `;
+
+    productContainer.appendChild(itemContainer);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadCart();
+  displayCartItems();
+});
