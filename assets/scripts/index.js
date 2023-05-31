bb6092620e843aedd763d3d6f29bffb334640380
= <=>
2023-05-31 19:00:00 +0400

    add to cart added

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -79,9 +79,11 @@ sideBarCloseBtn.addEventListener("click", () => {
 
 // BASKET START
 
-const itemContainer = document.querySelector(".item-container");
-const productContainer = document.querySelector(".navbar-cart-product");
-const subtotal = document.querySelector(".float-right");
+// const itemContainer = document.querySelector(".item-container");
+// const productContainer = document.querySelector(".navbar-cart-product");
+// const subtotal = document.querySelector(".float-right");
+
+// const addToCartButtons = document.querySelectorAll(".add-to-cart");
 
 const products = [
   {
@@ -89,7 +91,7 @@ const products = [
     name: "Norwegg Chair",
     price: 40.0,
     category: "Chairs",
-    image: "./assets/images/greenchairzoomedout.jpg",
+    image1: "./assets/images/greenchairzoomedout.jpg",
     count: 0,
   },
   {
@@ -150,98 +152,154 @@ const products = [
   },
 ];
 
-function generateProduct(products) {
-  const itemContainer = document.createElement("div");
-  itemContainer.className = "item-container";
+const itemContainer = document.querySelector(".item-container");
+const productContainer = document.querySelector(".navbar-cart-product");
+const subtotal = document.querySelector(".float-right");
 
-  itemContainer.innerHTML = `<div class="item-container">
-  <a href=""><img class="itemimg" src="${products.image}" alt="" /></a>
-  <div class="item-content">
-    <a class="closebtn-deleteitem" href="#">
-      <button class="closebtn-deleteitem" type="button">
-        <i class="fa-solid fa-xmark"></i>
-      </button>
-    </a>
-    <div class="pl-3">
-      <a class="navbar-cart-product" href="">${products.name}</a
-      ><small class="d-block text-muted">Quantity: ${products.count} </small
-      ><strong class="d-block text-sm">$${products.price}.00</strong>
-    </div>
-  </div>
-</div>`;
-  return itemContainer;
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
+
+addToCartButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const productId = parseInt(event.target.dataset.productId);
+    const product = products.find((p) => p.id === productId);
+
+    if (product) {
+      // Increment the quantity of the selected product
+      product.count++;
+
+      const itemContainer = document.createElement("div");
+      itemContainer.classList.add("item-container");
+
+      itemContainer.innerHTML = `
+        <a href=""><img class="itemimg" src="${product.image}" alt="" /></a>
+        <div class="item-content">
+          <a class="closebtn-deleteitem" href="#">
+            <button class="closebtn-deleteitem" type="button">
+              <i class="fa-solid fa-xmark"></i>
+            </button>
+          </a>
+          <div class="pl-3">
+            <a class="navbar-cart-product" href="">${product.name}</a>
+            <small class="d-block text-muted">Quantity: ${product.count}</small>
+            <strong class="d-block text-sm">$${product.price.toFixed(
+              2
+            )}</strong>
+          </div>
+        </div>
+      `;
+
+      productContainer.appendChild(itemContainer);
+    } else {
+      console.log(`Product with ID ${productId} not found.`);
+    }
+  });
+});
+
+function saveCart() {
+  sessionStorage.setItem("shoppingCart", JSON.stringify(cart));
 }
 
-function createProduct(product) {
-  const itemContainer = document.createElement("div");
-  itemContainer.classList.add("item-container");
-
-  const imageLink = document.createElement("a");
-  imageLink.href = "";
-
-  const itemImage = document.createElement("img");
-  itemImage.classList.add("itemimg");
-  itemImage.src = product.image;
-  itemImage.alt = "";
-  imageLink.appendChild(itemImage);
-
-  itemContainer.appendChild(imageLink);
-
-  const itemContent = document.createElement("div");
-  itemContent.classList.add("item-content");
-
-  const closeBtnLink = document.createElement("a");
-  closeBtnLink.classList.add("closebtn-deleteitem");
-  closeBtnLink.href = "#";
-
-  const closeBtn = document.createElement("button");
-  closeBtn.classList.add("closebtn-deleteitem");
-  closeBtn.type = "button";
-
-  const closeIcon = document.createElement("i");
-  closeIcon.classList.add("fa-solid", "fa-xmark");
-  closeBtn.appendChild(closeIcon);
-
-  closeBtnLink.appendChild(closeBtn);
-
-  itemContent.appendChild(closeBtnLink);
-
-  const itemDetails = document.createElement("div");
-  itemDetails.classList.add("pl-3");
-
-  const productLink = document.createElement("a");
-  productLink.classList.add("navbar-cart-product");
-  productLink.href = "";
-  productLink.innerText = product.name;
-
-  const quantity = document.createElement("small");
-  quantity.classList.add("d-block", "text-muted");
-  quantity.innerText = `Quantity: ${product.count}`;
-
-  const price = document.createElement("strong");
-  price.classList.add("d-block", "text-sm");
-  price.innerText = `$${product.price}.00`;
-
-  itemDetails.appendChild(productLink);
-  itemDetails.appendChild(quantity);
-  itemDetails.appendChild(price);
-
-  itemContent.appendChild(itemDetails);
-
-  itemContainer.appendChild(itemContent);
-
-  productContainer.appendChild(itemContainer);
+function loadCart() {
+  cart = JSON.parse(sessionStorage.getItem("shoppingCart"));
 }
+if (sessionStorage.getItem("shoppingCart") != null) {
+  loadCart();
+}
+
+// function generateProduct(products) {
+//   const itemContainer = document.createElement("div");
+//   itemContainer.className = "item-container";
+
+//   itemContainer.innerHTML = `<div class="item-container">
+//   <a href=""><img class="itemimg" src="${products.image}" alt="" /></a>
+//   <div class="item-content">
+//     <a class="closebtn-deleteitem" href="#">
+//       <button class="closebtn-deleteitem" type="button">
+//         <i class="fa-solid fa-xmark"></i>
+//       </button>
+//     </a>
+//     <div class="pl-3">
+//       <a class="navbar-cart-product" href="">${products.name}</a
+//       ><small class="d-block text-muted">Quantity: ${products.count} </small
+//       ><strong class="d-block text-sm">$${products.price}.00</strong>
+//     </div>
+//   </div>
+// </div>`;
+//   return itemContainer;
+// }
+
+// function createProduct(product) {
+//   const itemContainer = document.createElement("div");
+//   itemContainer.classList.add("item-container");
+
+//   const imageLink = document.createElement("a");
+//   imageLink.href = "";
+
+//   const itemImage = document.createElement("img");
+//   itemImage.classList.add("itemimg");
+//   itemImage.src = product.image;
+//   itemImage.alt = "";
+//   imageLink.appendChild(itemImage);
+
+//   itemContainer.appendChild(imageLink);
+
+//   const itemContent = document.createElement("div");
+//   itemContent.classList.add("item-content");
+
+//   const closeBtnLink = document.createElement("a");
+//   closeBtnLink.classList.add("closebtn-deleteitem");
+//   closeBtnLink.href = "#";
+
+//   const closeBtn = document.createElement("button");
+//   closeBtn.classList.add("closebtn-deleteitem");
+//   closeBtn.type = "button";
+
+//   const closeIcon = document.createElement("i");
+//   closeIcon.classList.add("fa-solid", "fa-xmark");
+//   closeBtn.appendChild(closeIcon);
+
+//   closeBtnLink.appendChild(closeBtn);
+
+//   itemContent.appendChild(closeBtnLink);
+
+//   const itemDetails = document.createElement("div");
+//   itemDetails.classList.add("pl-3");
+
+//   const productLink = document.createElement("a");
+//   productLink.classList.add("navbar-cart-product");
+//   productLink.href = "";
+//   productLink.innerText = product.name;
+
+//   const quantity = document.createElement("small");
+//   quantity.classList.add("d-block", "text-muted");
+//   quantity.innerText = `Quantity: ${product.count}`;
+
+//   const price = document.createElement("strong");
+//   price.classList.add("d-block", "text-sm");
+//   price.innerText = `$${product.price}.00`;
+
+//   itemDetails.appendChild(productLink);
+//   itemDetails.appendChild(quantity);
+//   itemDetails.appendChild(price);
+
+//   itemContent.appendChild(itemDetails);
+
+//   itemContainer.appendChild(itemContent);
+
+//   productContainer.appendChild(itemContainer);
+
+//   return productContainer;
+// }
 
 // function renderAllProducts() {
 //   products.forEach((product) => {
-//     const productItem = generateProduct(product);
+//     const productItem = createProduct(product);
 //     productContainer.appendChild(productItem);
 //   });
 // }
 // renderAllProducts();
 
-const deleteBtn = document
-  .querySelector(".closebtn-deleteitem")
-  .querySelector(".fa-xmark");
-console.log(deleteBtn);
+// const deleteBtn = document
+//   .querySelector(".closebtn-deleteitem")
+//   .querySelector(".fa-xmark");
+// console.log(deleteBtn);
