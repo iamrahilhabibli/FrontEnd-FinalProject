c74c6d770ff3d55c5adfecca1f7f9624a80c2744
Rahil Habibli <rahilah@code.edu.az>
2023-05-31 23:29:12 +0400

    loading basket from different pages done

--- assets/scripts/contact.js
+++ assets/scripts/contact.js
@@ -2,3 +2,88 @@ var image = document.getElementsByClassName("contactparallaximage");
 new simpleParallax(image);
 
 var map = L.map("map").setView([40.4093, 49.8671], 13);
+
+function loadCart() {
+  const savedCart = JSON.parse(localStorage.getItem("shoppingCart"));
+  if (savedCart) {
+    cart = savedCart;
+  }
+}
+function displayCartItems() {
+  const productContainer = document.querySelector(".navbar-cart-product");
+  const subtotalElement = document.querySelector(".float-right");
+
+  productContainer.innerHTML = ""; // Clear existing items
+
+  let subtotal = 0;
+
+  for (const item of cart) {
+    const itemContainer = document.createElement("div");
+    itemContainer.classList.add("item-container");
+
+    itemContainer.innerHTML = `
+        <a href=""><img class="itemimg" src="${item.image}" alt="" /></a>
+        <div class="item-content">
+          <a class="closebtn-deleteitem" href="#">
+            <button class="closebtn-deleteitem" type="button">
+              <i class="fa-solid fa-xmark"></i>
+            </button>
+          </a>
+          <div class="pl-3">
+            <a class="navbar-cart-product" href="">${item.name}</a>
+            <small class="d-block text-muted">Quantity: ${item.count}</small>
+            <strong class="d-block text-sm">$${item.price.toFixed(2)}</strong>
+          </div>
+        </div>
+      `;
+
+    productContainer.appendChild(itemContainer);
+
+    subtotal += item.price * item.count;
+  }
+
+  subtotalElement.textContent = `$${subtotal.toFixed(2)}`;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadCart();
+  displayCartItems();
+});
+
+const shoppingCartIcon = document.querySelector(".fa-bag-shopping");
+const modalCart = document.querySelector(".modal-dialog");
+const modal = document.querySelector(".modal");
+const closeBtn = document
+  .querySelector(".modal-header")
+  .querySelector(".closebtn");
+
+const sideBarModal = document.querySelector(".sidebar-modal");
+const sideBarModalDialog = document
+  .querySelector(".sidebar-modal")
+  .querySelector(".modal-dialog");
+const sideBarCloseBtn = document
+  .querySelector(".sidebar-modal")
+  .querySelector(".modal-header")
+  .querySelector(".closebtn");
+const staggeredBar = document.querySelector(".fa-bars-staggered");
+const modalContent = document
+  .querySelector(".modal-dialog")
+  .querySelector("modal-content");
+
+shoppingCartIcon.addEventListener("click", () => {
+  modal.style.display = "block";
+  modalCart.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+  modalCart.style.display = "none";
+});
+staggeredBar.addEventListener("click", () => {
+  sideBarModal.style.display = "block";
+  sideBarModalDialog.style.display = "block";
+});
+sideBarCloseBtn.addEventListener("click", () => {
+  sideBarModal.style.display = "none";
+  sideBarModalDialog.style.display = "none";
+});
